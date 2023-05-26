19faf2c00b8eadb583e93f7a5d7631cfa4cf6fc7
RizioDev <fabrizioalderete23@gmail.com>
2023-05-26 17:10:40 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Pago from "./components/Pago";
 import Select from "./components/Select";
-import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <>
-      <Nav />
       <Select />
     </>
   );
